919080f99554877a983d999f0a12f0bfcb060be2
Modesto Felipe <fekimodesto04@gmail.com>
2026-05-26 18:48:26 -0300

    feat: adiciona includes, defines e variáveis globais do firmware

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,56 +1,38 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
 
-#define BTN_BOM  18
-#define BTN_RUIM 19
-#define LED_VERDE 25
-#define LED_VERMELHO 27
+#define BTN_BOM       12
+#define BTN_MODERADO  13
+#define BTN_RUIM      14
+#define LED_VERDE     25
+#define LED_AMARELO   26
+#define LED_VERMELHO  27
+#define BUZZER        32
+
+const char* WIFI_SSID     = "Wokwi-GUEST";
+const char* WIFI_PASSWORD = "";
+
+const char* MQTT_BROKER = "broker.hivemq.com";
+const int   MQTT_PORT   = 1883;
+const char* MQTT_TOPIC  = "airwatch/leitura";
+const char* MQTT_CLIENT = "airwatch-esp32-solsticio";
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+WiFiClient        wifiClient;
+PubSubClient      mqtt(wifiClient);
+WebServer         server(80);
 
-int aqi = 50;
-String qualidade = "BOA";
+struct Leitura {
+  int    aqi;
+  String status;
+  String timestamp;
+};
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(BTN_BOM, INPUT_PULLUP);
-  pinMode(BTN_RUIM, INPUT_PULLUP);
-  pinMode(LED_VERDE, OUTPUT);
-  pinMode(LED_VERMELHO, OUTPUT);
-  digitalWrite(LED_VERDE, LOW);
-  digitalWrite(LED_VERMELHO, LOW);
-
-  lcd.init();
-  lcd.backlight();
-  lcd.print("AirWatch IoT");
-  lcd.setCursor(0, 1);
-  lcd.print("Use botoes");
-  delay(2000);
-  lcd.clear();
-}
-
-void loop() {
-  if (digitalRead(BTN_BOM) == LOW) {
-    delay(300);
-    aqi = 50;
-    qualidade = "BOA";
-    digitalWrite(LED_VERDE, HIGH);
-    digitalWrite(LED_VERMELHO, LOW);
-    lcd.clear();
-    lcd.print("AQI: 50 - BOA");
-    lcd.setCursor(0, 1);
-    lcd.print("Ar Saudavel");
-  }
-  if (digitalRead(BTN_RUIM) == LOW) {
-    delay(300);
-    aqi = 250;
-    qualidade = "RUIM";
-    digitalWrite(LED_VERDE, LOW);
-    digitalWrite(LED_VERMELHO, HIGH);
-    lcd.clear();
-    lcd.print("AQI: 250 - RUIM");
-    lcd.setCursor(0, 1);
-    lcd.print("Evite atividades");
-  }
-  delay(100);
-}
+Leitura leituraAtual  = { 0, "AGUARDANDO", "--" };
+Leitura historico[10];
+int     totalLeituras = 0;
+unsigned long ultimaLeitura = 0;
